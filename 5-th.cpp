cf11e21e61794f4eb7ebe16ac3d89f61aa929830
KATAROTA <112807542+KATAROTA@users.noreply.github.com>
2022-09-11 06:28:03 -0600

    Add files via upload

---
+++ 5-th.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	string name = " ";
+	int age =0;
+	cout << "как вас зовут? " << endl;
+	cin >> name;
+	cout << "ваш возраст: " << endl;
+	cin >> age;
+	cout << "ваше имя: " << name <<"." << " Ваш возраст: " << age << endl;
+	return 0;
+	
+	
+}
