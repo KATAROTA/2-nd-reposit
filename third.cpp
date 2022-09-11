a13e06f477377aa636128d9488d74b85a7fdc819
KATAROTA <112807542+KATAROTA@users.noreply.github.com>
2022-09-11 06:02:05 -0600

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	
+	string name = "";
+	int number = 0;
+	cout << "input integer number" << endl;
+	cin >> number;
+	cout << "Input ur name " << endl;
+	cin >> name;
+	cout << "user " << name << " input number " <<number <<endl;
+	
+}
